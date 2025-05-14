502809eed415e9e556ee0ef870468064875b2297
William Vance <william.vance@fhnw.edu>
2025-05-14 08:06:21 -0600

    added the SQL database

---
+++ Planning/FinalProject.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql
+-- Generation Time: May 14, 2025 at 02:05 PM
+-- Server version: 9.2.0
+-- PHP Version: 8.2.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `FinalProject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Admins`
+--
+
+CREATE TABLE `Admins` (
+  `adminName` text NOT NULL,
+  `adminWallets` text NOT NULL,
+  `id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Admins`
+--
+
+INSERT INTO `Admins` (`adminName`, `adminWallets`, `id`) VALUES
+('Owner', '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Events`
+--
+
+CREATE TABLE `Events` (
+  `eventLocation` text NOT NULL,
+  `eventDescription` text NOT NULL,
+  `eventTime` text NOT NULL,
+  `id` int NOT NULL,
+  `adminWallets` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `Events`
+--
+
+INSERT INTO `Events` (`eventLocation`, `eventDescription`, `eventTime`, `id`, `adminWallets`) VALUES
+('1209 Harrison', 'Created by - Vance MTG tourny', '9:00 PM MST', 1, '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
